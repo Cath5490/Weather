efd5d418cc743fc22a34b59ba38160d1a430d0f5
Cath5490 <96473031+Cath5490@users.noreply.github.com>
2022-03-10 17:34:52 +0100

    date now from API

--- src/js.js
+++ src/js.js
@@ -1,31 +1,3 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()]; // 0 and 6
-
-let hour = now.getHours();
-if (hour < 10) {
-  hour = "0" + hour;
-} else {
-  hour = hour;
-}
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = "0" + minute;
-} else {
-  minute = minute;
-}
-
-let h1 = document.querySelector("h1");
-h1.innerHTML = `${day} ${hour}:${minute}`;
-
 function search(cityName) {
   let apiKey = "c26cb2147528c68477d823cc1d5509f4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
@@ -38,8 +10,41 @@ function city(event) {
   search(cityName);
 }
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()]; // 0 and 6
+
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = "0" + hour;
+  } else {
+    hour = hour;
+  }
+
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = "0" + minute;
+  } else {
+    minute = minute;
+  }
+  return `${day} ${hour}:${minute}`;
+}
+
 function showWeather(response) {
-  console.log(response.data);
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
   document.querySelector("h2").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.main.temp
